08bf28f4913f225906fca647a5b3b555556dbda9
lsnk_ute <nhatkhue2006@gmail.com>
2025-08-25 14:20:31 +0700

    Update 03_Book.cpp

--- 03_Book.cpp
+++ 03_Book.cpp
@@ -2,9 +2,18 @@
 using namespace std;   
 
 class Book {
-    public:
+    private: 
         string title;
         string author;
+    public:
+        Book (string t, string au) {    //nếu dùng get & set thì chỉ cần Book () {} là được có thêm thì cũng không sao
+            title = t;
+            author = au;
+        }
+        void setTitle(string t) {title = t;}            
+        string getTitle() {return title;}
+        void setAuthor(string au) {author = au;}
+        string setAuthor () {return author;}
 
         void displayInfo() {
             cout << "Title: " << title << endl;
@@ -13,9 +22,9 @@ class Book {
     };
 
 int main() {
-    Book book1;
-    book1.title = "C++ Programming";
-    book1.author = "Bjarne Stroustrup";
+    Book book1("","");
+    book1.setTitle("C++ Programming");
+    book1.setAuthor("Bjarne Stroustrup");
     book1.displayInfo();
     return 0;
 }
